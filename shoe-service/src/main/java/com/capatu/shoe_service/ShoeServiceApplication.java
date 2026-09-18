3e72b6d5047c619da14d93d65d1a886db9865f42
handika <handikaalfalaq01@gmail.com>
2026-09-18 22:40:58 +0700

    init

---
+++ shoe-service/src/main/java/com/capatu/shoe_service/ShoeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.capatu.shoe_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeServiceApplication.class, args);
+	}
+
+}
